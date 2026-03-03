7eb41a0f4a6357c5d1672b83fe7ba7aeea4483f2
Lilian Natalie <lilian.natalie.c@gmail.com>
2026-03-03 10:47:58 +0100

    Pridanie MAIN/SIDE
    Kde MAIN sluzi na hotovu stranku a SIDE sluzi na napady

---
+++ MAIN/modules/parts/head.php
@@ -0,0 +1,14 @@
+<!-- MAIN SET HTML -->
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+<!-- META DATA -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+<!-- WEB ICON -->
+
+<!-- GOOGLE FONTS -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<!-- MAIN CSS -->
+<link rel="stylesheet" type="text/css" href="./assets/css/root.css">
